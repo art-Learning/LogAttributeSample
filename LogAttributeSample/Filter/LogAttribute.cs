739b449c8285b3eaf3c659c70698b2ec257cc6f0
art <art@cowell.com.tw>
2016-08-20 15:32:15 +0800

    LogAttribute done

--- LogAttributeSample/Filter/LogAttribute.cs
+++ LogAttributeSample/Filter/LogAttribute.cs
@@ -1,21 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace LogAttributeSample.Filter
 {
-    public class LogAttribute : ActionFilterAttribute
+    public sealed class LogAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            //Server.MapPath("~/Content/test.zip")
+
             base.OnActionExecuting(filterContext);
+            File.AppendAllText(HostingEnvironment.MapPath("~/App_Data") + "/ActionFilter.log",
+                "【OnActionExecuting】執行時間：" +
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine);
         }
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            base.OnActionExecuted(filterContext);
+            Thread.Sleep(1000);
+            File.AppendAllText(HostingEnvironment.MapPath("~/App_Data") + "/ActionFilter.log",
+                "【OnActionExecuted】執行時間：" +
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Thread.Sleep(1000);
+            File.AppendAllText(HostingEnvironment.MapPath("~/App_Data") + "/ActionFilter.log",
+                "【OnResultExecuting】執行時間：" +
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Thread.Sleep(1000);
+            File.AppendAllText(HostingEnvironment.MapPath("~/App_Data") + "/ActionFilter.log",
+                "【OnResultExecuted】執行時間：" +
+                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine);
         }
     }
 }
